33b7c6f961ac5e360056a893e0962ea06dd362e8
HUYNH KIM HIEU <89566149+goldenace21@users.noreply.github.com>
2022-05-12 11:03:43 +0700

    ok

---
+++ _01_relational_database/exercise/create_table.sql
@@ -0,0 +1,17 @@
+create database bai_tap;
+use bai_tap;
+CREATE TABLE class (
+    id INT,
+    `name` VARCHAR(50)
+);
+insert into class values(1,"hieu");
+insert into class values(2,"linh");
+
+CREATE TABLE student (
+    id INT,
+    `name` VARCHAR(50),
+    age INT,
+    nation VARCHAR(50)
+);
+insert into student values(2,"linh",15,"VietNam");
+insert into student values(4,"quang",15,"Japan");
